6ba8c0c4703df7630afe0485cee35f002f763195
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-14 11:26:01 +0530

    refactored to return number as String

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,6 @@ package com.kata.fizzbuzz;
 
 class FizzBuzz {
     String getFizzBuzz(int number) {
-        if(number == 2)
-            return "2";
-        return "1";
+        return String.valueOf(number);
     }
 }
