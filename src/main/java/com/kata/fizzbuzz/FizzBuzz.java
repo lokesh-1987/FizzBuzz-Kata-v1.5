8127d1e7bbd341ebe89e122873ff0452b55ea650
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-14 16:32:14 +0530

    extracted the logic for the multiples of 5 in a method called isBuzzNumber

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -8,7 +8,7 @@ class FizzBuzz {
     String getFizzBuzz(int number) {
         if (isFizzNumber(number)) {
             return FIZZ;
-        } else if (number % 5 == 0){
+        } else if (isBuzzNumber(number)){
             return BUZZ;
         }
         return String.valueOf(number);
@@ -16,5 +16,9 @@ class FizzBuzz {
 
     private boolean isFizzNumber(int number) {
         return number % 3 == 0;
+    }
+
+    private boolean isBuzzNumber(int number) {
+        return number % 5 == 0;
     }
 }
